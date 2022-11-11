d1438f9634dd6800a05171d85c34c7c7176338fa
rellarain <aurellarainwalker@gmail.com>
2022-11-11 00:20:16 -0500

    11/11

--- src/components/Dash.js
+++ src/components/Dash.js
@@ -9,10 +9,28 @@ export default function Dashboard() {return (
 
 function UserConsole() {return (
     <div id='userConsole'>
-        <div id='userConsoleMeta'></div>
+        <div id='userConsoleMeta'>
+            <br/>
+            Department title <br/>
+            <br/>
+            News: birthdays, updates, reminders <br/>
+            Online: employees, supervisors, team <br/>
+            Teams: rosters <br/>
+            Schedule: <br/>
+            Projects: tasks<br/>
+            Feedback: pending, processing, processed<br/>
+            Bulletin: discussions, polls<br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+        </div>
         <div id='userConsoleDesk'></div>
 
             
     </div>
 )}
+
+
 
--- src/components/HelpDesk.js
+++ src/components/HelpDesk.js
@@ -2,7 +2,15 @@ import React from 'react';
 
 export default function HelpDesk() {return (
     <div id="helpDesk"> 
+        <div className='helpChatWindow'>
+            <section></section>
+            <section></section>
+            <section></section>
+        </div>
+        
+        <div id='helpDeskFooter'>
 
+        </div>
     </div>)}
 
 
