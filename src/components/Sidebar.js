f666596e24b5d418cb0ac573aa6f1ab3fedb0acf
rellarain <aurellarainwalker@gmail.com>
2023-01-14 21:48:41 -0500

    1/14/22

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -3,14 +3,14 @@ import React from 'react';
 
 export default function Sidebar() {return (
     <div id="sidebar">
-        <QuickTools />
+        <Toolkit />
     </div>
 )}
 
 
-function QuickTools() {return (
-    <div id="quickTools">
-        <div id='quickToolsHead'>
+function Toolkit() {return (
+    <div id="toolkit">
+        <div id='toolkitHead'>
             <span alt='Agenda' className='openTab'></span>
             <span alt='Tracking'></span>
             <span alt='Library'></span>
@@ -21,7 +21,8 @@ function QuickTools() {return (
             <span alt='Simulator'></span>
             <span alt='Settings'></span>
         </div>
-        <div id='quickToolsTitle'>Quick Tools</div>
-        <div id='quickToolsBody'></div>
+        <div id='toolkitTitle'>Toolkit</div>
+        <div id='toolkitProgress'><span id='toolkitProgressBar'></span></div>
+        <div id='toolkitBody'></div>
     </div>
 )}
--- src/components/UserConsole.js
+++ src/components/UserConsole.js
@@ -13,30 +13,62 @@ export default function UserConsole() {return (
             <br/>
             Department title <br/>
             <br/>
-            News: birthdays, updates, reminders <br/>
-            Online: employees, supervisors, team <br/>
-            Teams: rosters <br/>
-            Schedule: team hours, supervisor hours<br/>
-            Projects: tasks<br/>
-            Feedback: pending, processing, processed<br/>
-            Bulletin: discussions, polls<br/>
-            Calendar: week, month, quarter, year<br/>
+            
             <br/>
-            Steps: in assessments or submissions, there is a component detailing the steps of the process, and what step you're on<br/>
             <br/>
             To-Do:<br/>
             <br/>
             <br/>
             <br/>
             <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
+            <ul>
+                <li>ORGANIZATIONAL</li>
+                <li>
+                    DEPARTMENT<br/>
+                    News: birthdays, updates, reminders <br/>
+                    Bulletin: discussions, polls <br/>
+                    Online: employees, supervisors, team <br/>
+                </li>
+                <li>
+                    TEAMS <br/>
+                    Roster: <br/>
+                    Calendar: week, month, quarter, year <br/>
+                    Schedule: team hours, supervisor hours <br/>
+                    Queues: feedback, <br/>
+                    Assignments: projects, tasks <br/>
+                    Productivity: pending, processed, awaiting <br/>
+                </li>
+                <li></li>
+                <li></li>
+            </ul>
+            <ul>
+                <li>ASSESSMENTS</li>
+                <li>Progress: previous steps, the step you are on, next steps</li>
+                <li></li>
+                <li></li>
+            </ul>
+            <ul>
+                <li>g</li>
+                <li></li>
+                <li></li>
+                <li></li>
+            </ul>
+            <ul>
+                <li>g</li>
+                <li></li>
+                <li></li>
+                <li></li>
+            </ul>
+            <ul>
+                <li>g</li>
+                <li></li>
+                <li></li>
+                <li></li>
+            </ul>
         </div>
         <div id='userConsoleDesk'>
-            Training: <br/>
+            Training: assessments, reading, projects, <br/>
+            <br/>
             Management: <br/>
             Break Time: <br/>
             Designer: <br/>
@@ -51,7 +83,7 @@ export default function UserConsole() {return (
             To-Do:<br/>
             - add window actions: pause, close<br/>
             - add tabs for steps in a process<br/>
-            <br/>
+            - <br/>
             <br/>
             <br/>
             <br/>
@@ -107,6 +139,14 @@ function ConsoleHelp() {return(
             <br/>
             <br/>
             Chat: contacting an employee trained on this topic<br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
         </div>
         <div id='userConsoleHelpButton'>
             <span>?</span>
