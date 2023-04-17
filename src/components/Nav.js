962bad1ada44968a9948fd7f1aa380ee9d43e9a3
rellarain <aurellarainwalker@gmail.com>
2023-04-16 21:14:25 -0400

    4/16/23 x5

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -3,7 +3,7 @@ import React from 'react';
 
 export default function Navigation() {return (
     // navCropClosed navCropPage navCropOpen navCropTools
-    <nav id='navCrop' className='navCropOpen'>
+    <nav id='navCrop' className='navCropClosed'>
         {/* Nav Button
                 
             Navigation Console: 
